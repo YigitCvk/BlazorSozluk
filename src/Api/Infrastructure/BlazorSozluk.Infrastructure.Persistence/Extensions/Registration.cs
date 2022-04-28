876e3a9024fe4907de6ae2f2b0c76eacd8dbe038
Yiğit Ç <75942835+YigitCvk@users.noreply.github.com>
2022-04-28 04:30:45 +0300

    Add project files.

---
+++ src/Api/Infrastructure/BlazorSozluk.Infrastructure.Persistence/Extensions/Registration.cs
@@ -0,0 +1,30 @@
+﻿using BlazorSozluk.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorSozluk.Infrastructure.Persistence.Extensions
+{
+    public static class Registration
+    {
+        public static IServiceCollection AddInfrastructureRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BlazorSozlukContext>(conf =>
+            {
+                var connStr = configuration["BlazorSozlukConnectionString"].ToString();
+                conf.UseSqlServer("", opt =>
+                {
+                    opt.EnableRetryOnFailure();
+                });
+            });
+            //var seedData = new SeedData();
+            //seedData.SeedAsync(configuration).GetAwaiter().GetResult();
+            return services;
+        }
+    }
+}
